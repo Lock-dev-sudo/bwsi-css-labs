3edfdd29772b1fd00e370693b8c0f9c806d2f437
Lock-dev-sudo <lvidgame@gmail.com>
2026-03-15 15:42:04 -0400

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -9,6 +9,20 @@ and prints the result to the terminal window.
 
 """
 
+def request_sanitized_operation(prompt: str) -> str:
+    """
+    Makes sure the operation is valid
+
+    returns: 
+        Str: the sanitized operation input by user. 
+    """
+    allowed = ["add", "subtract", "multiply", "divide"]
+    while True:
+        operation = input((prompt)).strip().lower()
+        if operation in allowed:
+            return operation
+        print("Invalid input. Enter one of the possible operation inputs.")
+
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
     Function that takes in two numbers and an operation (add, subtract, multiply, divide),
@@ -34,8 +48,6 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             return num1 / num2
         else:
             raise ValueError("Cannot divide by zero.")
-    else:
-        raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
 def request_sanitized_number(prompt: str) -> float:
     """
@@ -51,6 +63,8 @@ def request_sanitized_number(prompt: str) -> float:
         except ValueError:
             print("Invalid input. Enter a number.")
 
+
+
 def main():
     
     print(f"===== Simple Calculator =====")
@@ -58,7 +72,7 @@ def main():
     # Ask the user for sample input    
     num1 = request_sanitized_number("Enter the first number: ")
     num2 = request_sanitized_number("Enter the second number: ")
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = request_sanitized_operation(("Enter the operation (add, subtract, multiply, divide): "))
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
